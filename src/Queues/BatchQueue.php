e6a5026c418f1ee00892933be510815afe8d875e
Nahleen Antika <nantika@intouchinsight.com>
2026-06-12 09:54:27 -0400

    DEVOPS-275 Add new jobEcsPropertiesOverride for fargate and check for ec2 or fargate

--- src/Queues/BatchQueue.php
+++ src/Queues/BatchQueue.php
@@ -17,6 +17,7 @@ use Illuminate\Database\Connection;
 use Illuminate\Queue\DatabaseQueue;
 use Illuminate\Queue\Jobs\DatabaseJobRecord;
 use LukeWaite\LaravelQueueAwsBatch\Contracts\JobContainerOverrides;
+use LukeWaite\LaravelQueueAwsBatch\Contracts\JobEcsPropertiesOverride;
 use LukeWaite\LaravelQueueAwsBatch\Exceptions\JobNotFoundException;
 use LukeWaite\LaravelQueueAwsBatch\Exceptions\UnsupportedException;
 use LukeWaite\LaravelQueueAwsBatch\Jobs\BatchJob;
@@ -95,23 +96,27 @@ class BatchQueue extends DatabaseQueue
             ],
         ];
 
-        if (isset($job) && is_object($job) && $this->implementsJobContainerOverrides($job)) {
-            /** @var JobContainerOverrides $job */
-            $overrides = $job->getBatchContainerOverrides();
+        if (isset($job) && is_object($job)) {
+            if ($job instanceof JobEcsPropertiesOverride) {
+                /** @var JobEcsPropertiesOverride $job */
+                $overrides = $job->getBatchEcsPropertiesOverride();
 
-            if (isset($overrides)) {
-                $payload['containerOverrides'] = $overrides;
+                if (isset($overrides)) {
+                    $payload['ecsPropertiesOverride'] = $overrides;
+                }
+            } elseif ($job instanceof JobContainerOverrides) {
+                /** @var JobContainerOverrides $job */
+                $overrides = $job->getBatchContainerOverrides();
+
+                if (isset($overrides)) {
+                    $payload['containerOverrides'] = $overrides;
+                }
             }
         }
 
         $this->batch->submitJob($payload);
 
         return $jobId;
-    }
-
-    private function implementsJobContainerOverrides($job)
-    {
-        return $job instanceof JobContainerOverrides;
     }
 
     public function getJobById($id)
